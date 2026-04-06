151ef45b0ec467b282f9c2379e3810632e83fb57
jinkang-0 <jinkang.fang@gmail.com>
2026-04-06 15:55:59 -0700

    [patch] fix auth admin determination

--- app/admin-login/page.tsx
+++ app/admin-login/page.tsx
@@ -3,7 +3,6 @@ import styles from "./login.module.scss";
 import Footer from "@/components/ui/footer";
 import { getSession } from "@/actions/auth";
 import { redirect } from "next/navigation";
-import SessionRefresher from "@/components/auth/session-refresher";
 
 export default async function AdminLoginPage() {
   const session = await getSession();
@@ -15,7 +14,6 @@ export default async function AdminLoginPage() {
 
   return (
     <main className={styles.container}>
-      <SessionRefresher user={session.user!} />
       <article>
         <h4>Admin Login</h4>
         <ButtonGoogleSignIn />
