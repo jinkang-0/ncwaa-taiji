7d709bbc8d4dd6232adbe80316153d27dc2927f7
jinkang-0 <jinkang.fang@gmail.com>
2026-09-22 23:37:52 -0700

    update loading method to disable ssr

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,24 +1,13 @@
-import CMSLoader from "@/components/admin/loader";
+"use client";
+
+import dynamic from "next/dynamic";
+
+const CMSLoader = dynamic(() => import("@/components/admin/loader"), {
+  ssr: false,
+});
 
 export default function Page() {
-  return (
-    <html>
-      <head
-        dangerouslySetInnerHTML={{
-          __html: `
-          <head>
-              <meta charset="utf-8" />
-              <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-              <meta name="robots" content="noindex" />
-              <title>Content Manager</title>
-              <link href="admin/config.yml" type="text/yaml" rel="cms-config-url">
-          </head>        
-        `,
-        }}
-      />
-      <body>
-        <CMSLoader />
-      </body>
-    </html>
-  );
+  if (typeof window === "undefined") return;
+
+  return <CMSLoader />;
 }
--- components/admin/loader.tsx
+++ components/admin/loader.tsx
@@ -1,20 +1,10 @@
-"use client";
-
 import CMS from "decap-cms-app";
-import { useEffect, useState } from "react";
 import InfoPagePreview from "./info/info-page-preview";
 
+CMS.init();
+CMS.registerPreviewStyle("admin/global.css");
+CMS.registerPreviewTemplate("info", InfoPagePreview);
+
 export default function CMSLoader() {
-  const [initialized, setInitialized] = useState(false);
-
-  useEffect(() => {
-    if (initialized || typeof window === "undefined") return;
-    setInitialized(true);
-
-    CMS.init();
-    CMS.registerPreviewStyle("admin/global.css");
-    CMS.registerPreviewTemplate("info", InfoPagePreview);
-  }, []);
-
-  return <></>;
+  return <div />;
 }
